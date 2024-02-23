16d11447328745801852a23b3171fae1441cdf13
Roberto Oliveira <rvolive@gmail.com>
2024-02-23 13:01:59 -0300

    done with restaurants controller

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,8 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
+
   CUISINES = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+
   validates :name, :address, :category, :phone_number, presence: true
   validates :category, inclusion: { in: CUISINES }
 end
